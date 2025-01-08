85efd607486689ee5482b07159940be1718080af
osh0625 <dhtkdgur0@gmail.com>
2025-01-08 15:10:43 +0900

    map_marker update

--- map/static/js/script.js
+++ map/static/js/script.js
@@ -9,15 +9,14 @@ const locationInfoDiv = document.getElementById('location-info');  // 상태 메
  */
 function initMap() {
     const defaultLocation = { lat: 37.5665, lng: 126.9780 }; // 서울 시청
-    const marker_1 = { lat: 38.5665, lng: 126.9780 };
-    const marker_2 = { lat: 38.5665, lng: 126.9780 };
-    const marker_3 = { lat: 38.5665, lng: 126.9780 };
-    const marker_4 = { lat: 38.5665, lng: 126.9780 };
+    const marker_1 = { lat: 33.362247, lng: 126.5334223 };
+    const marker_2 = { lat: 33.5070772, lng: 126.4934311 };
+
 
     // Google 지도 초기화 및 기본 설정
     map = new google.maps.Map(document.getElementById('map'), {
-        center: osh,
-        zoom: 15,
+        center: marker_1,
+        zoom: 15-5,
         zoomControl: true,         // 줌 컨트롤
         mapTypeControl: true,      // 지도 유형 컨트롤
         scaleControl: true,        // 축척 컨트롤
@@ -27,26 +26,18 @@ function initMap() {
     });
 
     // 초기 위치에 마커 생성
-    marker_1 = new google.maps.Marker({
-        position: defaultLocation,
+    marker = new google.maps.Marker({
+        position: marker_1,
         map: map,
-        title: '현재 위치'
+        title: '백록담'
     });
-    marker_2 = new google.maps.Marker({
-        position: osh,
+
+    marker = new google.maps.Marker({
+        position: marker_2,
         map: map,
-        title: '현재 위치'
+        title: '제주공항'
     });
-    marker_3 = new google.maps.Marker({
-        position: osh,
-        map: map,
-        title: '현재 위치'
-    });
-    marker_4 = new google.maps.Marker({
-        position: osh,
-        map: map,
-        title: '현재 위치'
-    });
+
 
     infowindow = new google.maps.InfoWindow({
         content: "마커에 표시할 내용",
